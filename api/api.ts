da3197b4c4c51685f234426a88f38ee0fc35cab8
Dzmitry Shuysky <errfrom@yandex.ru>
2023-11-22 14:44:33 +0100

    feat(announce_auction): add corresponding ts types

--- api/api.ts
+++ api/api.ts
@@ -1,4 +1,13 @@
 import * as Purs from "../output/HydraAuctionOffchain.Api/index.js";
 
-export const announceAuction = async (walletApp: any, params: any): Promise<any> =>
-  Purs.announceAuction(walletApp)(params)();
+import type { 
+  AnnounceAuctionContractParams,
+  ContractOutput,
+  TransactionHash,
+  WalletApp
+} from "./types"
+
+export const announceAuction = async (
+  walletApp: WalletApp,
+  params: AnnounceAuctionContractParams
+): Promise<ContractOutput<TransactionHash>> => Purs.announceAuction(walletApp)(params)();
