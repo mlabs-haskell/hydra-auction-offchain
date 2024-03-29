5de543fafc3a6b9b5ffc6c856cfea9290a414bb0
Dzmitry Shuysky <errfrom@yandex.ru>
2024-03-29 18:08:34 +0100

    ci: fix plutip tests

--- src/Config.js
+++ src/Config.js
@@ -1,4 +1,4 @@
-export const _isMainnet = process.env.CARDANO_NETWORK === "mainnet";
+export const _isMainnet = (process.env.CARDANO_NETWORK ?? "mainnet") === "mainnet";
 export const _blockfrostApiKey = process.env.BLOCKFROST_API_KEY ?? "";
 export const _plutipEnvHostPort = process.env.PLUTIP_ENV_HOST_PORT ?? "localhost:8083";
 export const _demoHostPort = process.env.DEMO_HOST_PORT ?? "localhost:8080";
